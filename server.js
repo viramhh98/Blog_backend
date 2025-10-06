99585d6ca3f2f79bab934cdb7a24e5d5fee07427
Viram Shah <shahviramhh98@gmail.com>
2025-10-06 16:53:46 +0530

    versel.json added

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173","https://blog-frontend-q437.vercel.app/blogs"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
